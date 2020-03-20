c4b450e192c5304ced57239f5fe695799b43f44c
Dan Tannenbaum <dtannenbaum@MacBook-Pro.localdomain>
2020-03-20 19:26:37 -0400

    Cleanup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+
+
 from ev3dev2.button import Button
 from ev3dev2.sound import Sound
 from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D, Motor
@@ -22,6 +24,7 @@ from globals import *
 
 btn = Button()
 sound = Sound()
+
 
 turret = Motor(OUTPUT_A)
 attach = Motor(OUTPUT_D)
@@ -49,70 +52,6 @@ class MCTire(Wheel):
     """
     def __init__(self):
         Wheel.__init__(self, 100.3, 17)
-
-
-# def left(state):
-#     if state:
-#         debug_print('Left button pressed')
-#         turret.on(speed=45)
-#     else:
-#         debug_print('Left button released')
-#         turret.off()
-#     return 0
-   
-# def right(state):  # neater use of 'if' follows:
-#     if state:
-#         debug_print('right button pressed')
-#         turret.on(speed=-45)
-#     else:
-#         debug_print('right button released')
-#         turret.off()
-
-#     return 0
- 
-# def up(state):
-#     if state:
-#         debug_print('Up button pressed')
-#         attach.on(speed=5)
-#     else:
-#         debug_print('Up button released')
-#         attach.off()
-#     return 0
-   
-# def down(state):
-#     if state:
-#         debug_print('Down button pressed')
-#         attach.on(speed=-5)
-#     else:
-#         debug_print('Down button released')
-#         attach.off()
-#     return 0
-
-# def enter(state):
-#     if state:
-#         debug_print('Enter button pressed')
-#     else:
-#         debug_print('Enter button released')
-
-
-# def aaasetup():
-
-#     debug_print('aaasetup')
-#     print('hit a button')  
-#     btn.on_left = left
-#     btn.on_right = right
-#     btn.on_up = up
-#     btn.on_down = down
-#     btn.on_enter = enter
-
-#     while True:
-#         #debug_print(btn.buttons_pressed)
-#         if btn.check_buttons(buttons=['enter']):
-#             debug_print('enter button hit')
-#             break
-#         btn.process()
-#         time.sleep(0.01)
-
 
 def Main():
     # set the console just how we want it
