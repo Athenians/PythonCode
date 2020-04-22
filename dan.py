0b24c0400186121a2ecf3e84cd1d9d68de24ead7
kpu979 <59117124+kpu979@users.noreply.github.com>
2020-04-22 18:45:48 -0400

    new change

---
+++ dan.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+
+
+from ev3dev2.button import Button
+from ev3dev2.sound import Sound
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D, Motor
+from ev3dev2.motor import MoveTank, MoveDifferential
+from ev3dev2.motor import SpeedDPS, SpeedRPM, SpeedRPS, SpeedDPM, SpeedPercent, follow_for_ms
+from ev3dev2.wheel import Wheel
+from ev3dev2.sensor import INPUT_1, INPUT_2,  INPUT_4
+from ev3dev2.sensor.lego import TouchSensor, ColorSensor, GyroSensor
+
+import logging as log
+
+
+import os
+import sys
+import time
+
