f6c72b1b861777679d0960c43371e910e4c1fbb7
dbalaraman <63217075+dbalaraman@users.noreply.github.com>
2020-04-29 19:19:24 -0400

    Divya-update

--- divya.py
+++ divya.py
@@ -1,13 +1,21 @@
+
 n=int(5)
 for row in range(1,n+1):
     for col in range(1,n+1):
         print(row*col)
     print()
+<<<<<<< HEAD
 
 from ev3dev2.motor import OUTPUT_C, OUTPUT_B, MoveTank, MoveSteering, 
 from ev3dev2.sensor.lego import TouchSensor
 from time import sleep
 #Forms a square, moves front, makes another square, and goes back to initial area
+=======
+from ev3dev2.motor import OUTPUT_C, OUTPUT_B, MoveTank, MoveSteering, SpeedPercent
+from ev3dev2.sensor.lego import TouchSensor
+from time import sleep
+#Forms a square, moves front, makes another square, and goes back to initial ares
+>>>>>>> Divya-update
 tank_pair = MoveTank(OUTPUT_B, OUTPUT_C)
 
 
@@ -26,3 +34,11 @@ for x in range(4):
     sleep(1)
 tank_pair.on(left_speed=65, right_speed=65)
 sleep(1)
+<<<<<<< HEAD
+=======
+
+steer_pair = MoveSteering(OUTPUT_B, OUTPUT_C)
+# drive in a turn for 10 rotations of the outer motor
+steer_pair.on_for_rotations(steering=-60, speed=25, rotations=10)
+steer_pair.off()
+>>>>>>> Divya-update
