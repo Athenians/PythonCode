bb3981000b126974fd6317c9b7c3db5ad6e4965c
dbalaraman <63217075+dbalaraman@users.noreply.github.com>
2020-04-29 19:37:24 -0400

    Divya-update

--- myblocks.py
+++ myblocks.py
@@ -8,5 +8,8 @@ from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_4
 from ev3dev2.sensor.lego import TouchSensor, ColorSensor, GyroSensor
 # MOVE BLOCK
 tank_pair = MoveTank(OUTPUT_B, OUTPUT_C) 
-#no matter what speed always must be a negative integer
-tank_pair.on(left_speed=-15, right_speed=-15)
+#no matter what speed always must be a negative integer to go front,positive to go back
+#Fill in number wanted
+#Faster you go more distance you achieve
+tank_pair.on(left_speed=, right_speed=)
+
