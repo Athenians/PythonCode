a4637eb913d92ca61290f089f5b88192caf4baf1
dbalaraman <63217075+dbalaraman@users.noreply.github.com>
2020-07-07 15:59:29 -0400

    Finished move block

--- myblocks.py
+++ myblocks.py
@@ -8,16 +8,8 @@ from ev3dev2.wheel import Wheel
 from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_4
 from ev3dev2.sensor.lego import TouchSensor, ColorSensor, GyroSensor
 # MOVE BLOCK
-wheels = MoveTank(OUTPUT_B, OUTPUT_C) 
-#left_speed=
-#circumference=
-#right_speed=
-'''Format
-wheels.on_for_rotations(left_speed,right_speed, rotations, brake=True, block=True)
-'''
-wheels.on_for_rotations(5)
-'''
-How to add pi
-import math
-math.pi will print pi and add it as a variable  
+mtank = MoveTank(OUTPUT_B, OUTPUT_C) 
+def moveblock(mtank,speed,rotations):
+    mtank.on_for_rotations(speed, rotations, brake=True, block=True)
+
 
