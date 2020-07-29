f766126fb5680c512687bf0c4be7b37edfaef07b
kpu979 <59117124+kpu979@users.noreply.github.com>
2020-07-29 15:29:02 -0400

    dan_notes

---
+++ dan_notes.py
@@ -0,0 +1,21 @@
+
+#  these are my notes.
+
+#   pseudo code:
+
+#   Mission xx
+#   Place robot in starting position (manually)
+#   ensure arm is 0x0
+#   Move forward 30mm -- speed normal
+#   turn 90deg -- speed normal
+#   find line -- speed normal
+#   follow line for 300 mm -- speed normal
+#   turn -90 -- speed normal
+#   move forward 50mm -- speed fast
+#   arm to 20x0
+#   arm to 20x20
+#   complete mission
+#   go home
+#   while going home, arm to 0x0
+
+
