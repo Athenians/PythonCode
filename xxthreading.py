652645d25f97527e168152686687dc92657a4dd4
dbalaraman <63217075+dbalaraman@users.noreply.github.com>
2020-10-25 14:38:09 -0400

    threading

--- xxthreading.py
+++ xxthreading.py
@@ -15,3 +15,43 @@
 #    while bt.value() ==1:  # while button is pressed
 #        sleep(0.01)        # do nothing other than wait
 #Sound.beep()
+
+"""
+To start a thread
+x = threading.Thread(target=thread_function, args=(1,))
+x.start()
+
+Single Thread Format
+
+$ ./single_thread.py
+Main    : before creating thread
+Main    : before running thread
+Thread 1: starting
+Main    : wait for the thread to finish
+Main    : all done
+Thread 1: finishing
+
+Example of single thread
+
+import logging
+import threading
+import time
+
+def thread_function(name):
+    logging.info("Thread %s: starting", name)
+    time.sleep(2)
+    logging.info("Thread %s: finishing", name)
+
+if __name__ == "__main__":
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+
+    logging.info("Main    : before creating thread")
+    x = threading.Thread(target=thread_function, args=(1,))
+    logging.info("Main    : before running thread")
+    x.start()
+    logging.info("Main    : wait for the thread to finish")
+    # x.join()
+    logging.info("Main    : all done")
+"""
