0c5979f926946f7f5fd6e9ba157e85b78c0560aa
Divyashree Balaraman <divya.bvraman@gmail.com>
2021-09-02 16:04:55 -0400

    9/2/2021

--- missions.py
+++ missions.py
@@ -24,3 +24,55 @@ def calibrate(eve):
     eve.calibrategs()
     eve.calibratecs(10,2)
 
+def mission_bridge(eve):
+    eve.moveblock(20,20,200)
+    eve.turnblock(15,65)
+    eve.line_finder(10,10,'l','b')
+    #add attachment or stick to drop bridge
+    eve.left_motor.position = 0
+    eve.right_motor.position = 0 
+    try:
+        eve.athfollow_line(
+            #kp=2, ki=0.060, #kd=3,
+            kp=3, ki=0.00, kd=0,         # use this for change of directions speed = 10       
+            #kp=2, ki=0.000, #kd=0,           # use this for speed=20 on straight lines
+            speed=SpeedPercent(12),
+            cs_for_line = eve.csr,            
+            follow_left_edge=True,
+            sleep_time=0.002,
+            #follow_for=follow_until_line,cs_for_until = eve.csl, wb = 'b',tolerence=2
+            follow_for = follow_for_distance,distance = 1400
+            #follow_for=follow_for_forever
+            #follow_for=follow_for_ms,  ms=4500
+        
+        )
+    except LineFollowErrorTooFast:
+        eve.stop() 
+        raise
+    
+    eve.turnblock(15,10)
+    #food package dropped during this 
+    eve.turnblock(15,178)
+    #add attachment or stick to drop bridge
+    eve.left_motor.position = 0
+    eve.right_motor.position = 0 
+    try:
+        eve.athfollow_line(
+            #kp=2, ki=0.060, #kd=3,
+            kp=3, ki=0.00, kd=0,         # use this for change of directions speed = 10       
+            #kp=2, ki=0.000, #kd=0,           # use this for speed=20 on straight lines
+            speed=SpeedPercent(12),
+            cs_for_line = eve.csr,            
+            follow_left_edge=True,
+            sleep_time=0.002,
+            #follow_for=follow_until_line,cs_for_until = eve.csl, wb = 'b',tolerence=2
+            follow_for = follow_for_distance,distance = 1450
+            #follow_for=follow_for_forever
+            #follow_for=follow_for_ms,  ms=4500
+        
+        )
+    except LineFollowErrorTooFast:
+        eve.stop() 
+        raise
+
+    eve.moveblock(20,20,115)
