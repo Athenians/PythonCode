f1758a29ebb631c84c6efeb58847bc7b4784a8c8
kpu979 <kpu979@gmail.com>
2023-09-24 18:00:56 -0400

    9/24/2023

--- missions.py
+++ missions.py
@@ -22,8 +22,16 @@ import threading
 
 def mission_pickle_dragon(eve):
     eve.aaasetup()
-    eve.motor_mover(50,1,eve.attach)
-    eve.moveblock(30,30,190)
+
+    x = threading.Thread(target = eve.moveblock, args = (30,30,190,) )
+    y = threading.Thread(target = eve.motor_mover, args = (50,1,eve.attach,))
+
+    x.start()
+    y.start()
+
+    x.join()
+    y.join()
+
     eve.moveblock(-10,10,30)
     eve.moveblock(30,30,130)
     eve.moveblock(10,-10,30)
@@ -31,8 +39,26 @@ def mission_pickle_dragon(eve):
 
 def M02(eve):
     eve.aaasetup()    
-    eve.motor_mover(40,1.25,eve.attach)
-    eve.moveblock(20,20,600)
+
+    x = threading.Thread(target = eve.moveblock, args = (20,20,600,) )
+    y = threading.Thread(target = eve.motor_mover, args = (40,1.25,eve.attach,))
+
+    x.start()
+    y.start()
+
+    x.join()
+    y.join()
+
     for x in range (2):
-        eve.motor_mover(20,-1.5,eve.attach)
-        eve.motor_mover(20,1.5,eve.attach)
+        eve.motor_mover(20,-1,eve.attach)
+        eve.motor_mover(20,1,eve.attach)
+    eve.moveblock(20,20,600)
+
+def M03(eve):
+    eve.aaasetup()
+    eve.moveblock(30,30,605)
+    eve.moveblock(10,15,410)
+    eve.motor_mover(20,3.5,eve.attach)
+    eve.moveblock(10,10,80)
+    eve.motor_mover(20,-2.5,eve.attach)
+    eve.motor_mover(20,1.5,eve.attach)
